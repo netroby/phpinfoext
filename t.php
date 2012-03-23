1a3b886375ef053d585562f6cecdea1a1d346604
netroby <netroby@netroby.com>
2012-03-23 13:32:53 +0800

    增加清空redis缓存的功能

--- t.php
+++ t.php
@@ -42,19 +42,26 @@ if (isset($_GET['act']) && 'functions' === $_GET['act']) {
 } else if (isset($_GET['act']) && 'redis' === $_GET['act']) {
 	$redis  = new Redis();
 	$redis->connect('127.0.0.1', 6379);
+	if (isset($_GET['do']) && 'clear' === $_GET['do']) {
+		$redis->flushAll();
+		header('location:' . $_SERVER['HTTP_REFERER']);
+		exit();
+	}
 	// set a test value
 	$redis->setex('testkey', 720, 'i am value');
 	$all_keys = $redis->keys('*');
-	echo '<h2>所有的缓存内容</h2>';
-	echo '<table border="1">';
-	foreach ($all_keys as $v) 
-		echo '<tr><td>' . $v . "</td><td>" . $redis->get($v) . "</td></tr>";
-	echo '</table>';
+	echo '<h2><a href="' . $_SERVER['SCRIPT_NAME'] . '?act=redis&do=clear">清空所有缓存</a></h2>';
 	echo '<h2>Redis Info</h2>';
 	$info = $redis->info();
 	echo '<table border="1">';
 	foreach ($info as $k => $v) 
 		echo '<tr><td>' . $k . "</td><td>" . $v . "</td></tr>";
+	echo '</table>';
+
+	echo '<h2>所有的缓存内容</h2>';
+	echo '<table border="1">';
+	foreach ($all_keys as $v) 
+		echo '<tr><td>' . $v . "</td><td>" . $redis->get($v) . "</td></tr>";
 	echo '</table>';
 }
 ?>
